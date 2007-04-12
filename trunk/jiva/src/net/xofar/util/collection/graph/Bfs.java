0ee476ff94fd4c57bfc432a02073929613f6ccfb
pant.lalit <pant.lalit@4bbe0fb8-162d-0410-b77a-594aeffe64d9>
2007-04-12 21:36:27 +0000

    Graph implementation refinements

---
+++ trunk/jiva/src/net/xofar/util/collection/graph/Bfs.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2007 Lalit Pant <pant.lalit@gmail.com>
+ *
+ * The contents of this file are subject to the GNU General Public License 
+ * Version 2 or later (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.gnu.org/copyleft/gpl.html
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ */
+
+package net.xofar.util.collection.graph;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.xofar.util.collection.SmartMap;
+import net.xofar.util.collection.graph.Graph.VertexColor;
+
+/**
+ * Class that implements the breadth-first-search algorithm on a graph
+ * 
+ * @author lalitp
+ */
+public class Bfs<T>
+{
+
+    public SearchResults<T> bfs(Graph<T> graph, Vertex<T> root,
+            GraphVisitor<T> visitor)
+    {
+        Deque<Vertex<T>> open = new ArrayDeque<Vertex<T>>();
+        SearchResults<T> sr = new SearchResults<T>();
+        SearchState ss = new SearchState();
+
+        ss.setGray(root);
+        open.addLast(root);
+        while (!open.isEmpty()) {
+            Vertex<T> vertex = open.removeFirst();
+            visitor.visit(vertex);
+            ss.setBlack(vertex);
+
+            List<Vertex<T>> adjacents = graph.adjList(vertex);
+            for (Vertex<T> adjVertex : adjacents) {
+                if (ss.isBlack(adjVertex)) {
+                    continue;
+                }
+                if (ss.isGray(adjVertex)) {
+                    continue;
+                }
+                ss.setGray(adjVertex);
+                sr.setParent(adjVertex, vertex);
+                open.addLast(adjVertex);
+            }
+        }
+        return sr;
+    }
+    
+    class SearchState
+    {
+        Map<Vertex<T>, VertexColor> vState = new SmartMap<Vertex<T>, VertexColor>(
+                VertexColor.WHITE);
+        
+        public boolean isBlack(Vertex<T> vertex)
+        {
+            return vState.get(vertex).equals(VertexColor.BLACK);
+        }
+
+        public boolean isGray(Vertex<T> vertex)
+        {
+            return vState.get(vertex).equals(VertexColor.GRAY);
+        }
+
+        public void setGray(Vertex<T> vertex)
+        {
+            vState.put(vertex, VertexColor.GRAY);
+        }
+
+        public void setBlack(Vertex<T> vertex)
+        {
+            vState.put(vertex, VertexColor.BLACK);
+        }
+
+    }
+
+    static class SearchResults<T>
+    {
+        Map<Vertex<T>, Vertex<T>> parents = new HashMap<Vertex<T>, Vertex<T>>();
+
+        public void setParent(Vertex<T> child, Vertex<T> parent)
+        {
+            parents.put(child, parent);
+        }
+
+        @Override
+        public String toString()
+        {
+            return parents.toString();
+        }
+    }
+}
---
+++ trunk/jiva/src/net/xofar/util/collection/graph/Dfs.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2007 Lalit Pant <pant.lalit@gmail.com>
+ *
+ * The contents of this file are subject to the GNU General Public License 
+ * Version 2 or later (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.gnu.org/copyleft/gpl.html
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ */
+
+package net.xofar.util.collection.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import net.xofar.util.collection.SmartMap;
+import net.xofar.util.collection.graph.Graph.VertexColor;
+
+/**
+ * Class that implements the depth-first-search algorithm on a graph
+ * 
+ * @author lalitp
+ */
+class Dfs<T>
+{
+    private SearchResults sr;
+    private SearchState ss;
+
+    public SearchResults dfs(Graph<T> graph, GraphVisitor<T> visitor)
+    {
+        sr = new SearchResults();
+        ss = new SearchState();
+        Set<Vertex<T>> vertices = graph.getVertices();
+        for (Vertex<T> vertex : vertices) {
+            if (ss.isWhite(vertex)) {
+                dfs(graph, vertex, visitor);
+            }
+        }
+        return sr;
+    }
+
+    void dfs(Graph<T> graph, Vertex<T> vertex, GraphVisitor<T> visitor)
+    {
+        ss.setGray(vertex);
+        for (Vertex<T> adjVertex : graph.adjList(vertex)) {
+            if (ss.isWhite(adjVertex)) {
+                sr.setParent(adjVertex, vertex);
+                dfs(graph, adjVertex, visitor);
+            }
+            else if (ss.isGray(adjVertex)) {
+                sr.backEdge(vertex, adjVertex);
+            }
+            else if (ss.isBlack(adjVertex)) {
+                sr.crossOrFwdEdge(vertex, adjVertex);
+            }
+        }
+        visitor.visit(vertex); // kind of a post-order traversal
+        ss.setBlack(vertex);
+    }
+
+    class SearchState
+    {
+        Map<Vertex<T>, VertexColor> vState = new SmartMap<Vertex<T>, VertexColor>(
+                VertexColor.WHITE);
+
+        public boolean isBlack(Vertex<T> vertex)
+        {
+            return vState.get(vertex).equals(VertexColor.BLACK);
+        }
+
+        public boolean isWhite(Vertex<T> vertex)
+        {
+            return vState.get(vertex).equals(VertexColor.WHITE);
+        }
+
+        public boolean isGray(Vertex<T> vertex)
+        {
+            return vState.get(vertex).equals(VertexColor.GRAY);
+        }
+
+        public void setGray(Vertex<T> vertex)
+        {
+            vState.put(vertex, VertexColor.GRAY);
+        }
+
+        public void setBlack(Vertex<T> vertex)
+        {
+            vState.put(vertex, VertexColor.BLACK);
+        }
+
+    }
+
+    class SearchResults
+    {
+        Map<Vertex<T>, Vertex<T>> parents = new HashMap<Vertex<T>, Vertex<T>>();
+        Map<Vertex<T>, Vertex<T>> backEdges = new HashMap<Vertex<T>, Vertex<T>>();
+        Map<Vertex<T>, Vertex<T>> crossOrFwdEdge = new HashMap<Vertex<T>, Vertex<T>>();
+
+        public void setParent(Vertex<T> child, Vertex<T> parent)
+        {
+            parents.put(child, parent);
+        }
+
+        public void backEdge(Vertex<T> v1, Vertex<T> v2)
+        {
+            backEdges.put(v1, v2);
+        }
+
+        public void crossOrFwdEdge(Vertex<T> v1, Vertex<T> v2)
+        {
+            crossOrFwdEdge.put(v1, v2);
+        }
+
+        public boolean isTree()
+        {
+            return backEdges.isEmpty()
+                    ? true
+                    : false;
+        }
+
+        @Override
+        public String toString()
+        {
+            return parents.toString();
+        }
+    }
+}
